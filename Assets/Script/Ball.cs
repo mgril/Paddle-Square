fecd190fe4032c9062636c8704d5eb522580a161
mgril <112563882+mgril@users.noreply.github.com>
2023-09-19 12:32:26 +0200

    Initial commit
    Game Scene + Controlling the Ball ( position and vellocity +Bouncing off the Boundary) +Moving the Paddle

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour 
+{
+    [SerializeField, Min(0f)]
+	float
+		constantXSpeed = 8f,
+		constantYSpeed = 10f,
+		extents = 0.5f;
+
+	Vector2 position, velocity;
+
+    public float Extents => extents;
+	public Vector2 Position => position;
+
+    public void UpdateVisualization () =>
+		transform.localPosition = new Vector3(position.x, 0f, position.y);
+
+    public void Move () => position += velocity * Time.deltaTime;
+
+    public void StartNewGame ()
+	{
+		position = Vector2.zero;
+		UpdateVisualization();
+		velocity = new Vector2(constantXSpeed, -constantYSpeed);
+	}
+
+    public void BounceX (float boundary) // refraction en X lors d'un rebond 
+	{
+		position.x = 2f * boundary - position.x;
+		velocity.x = -velocity.x;
+	}
+
+	public void BounceY (float boundary)
+	{
+		position.y = 2f * boundary - position.y;
+		velocity.y = -velocity.y;
+	}
+
+}
+
---
+++ Assets/Script/Paddle.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour 
+{
+	[SerializeField, Min(0f)]
+	float
+		extents = 4f,
+		speed = 10f;
+
+    [SerializeField]
+	bool isAI;
+
+    public void Move (float target, float arenaExtents)
+	{
+		Vector3 p = transform.localPosition;
+        p.x = isAI ? AdjustByAI(p.x, target) : AdjustByPlayer(p.x);
+		float limit = arenaExtents - extents;
+		p.x = Mathf.Clamp(p.x, -limit, limit);
+		transform.localPosition = p;
+	}
+    float AdjustByAI (float x, float target)    //stupid AI with no prediction , difficulty only depends on its speed
+	{
+		if (x < target)
+		{
+			return Mathf.Min(x + speed * Time.deltaTime, target);
+		}
+		return Mathf.Max(x - speed * Time.deltaTime, target);
+	}
+    float AdjustByPlayer (float x)
+	{
+		bool goRight = Input.GetKey(KeyCode.RightArrow);
+		bool goLeft = Input.GetKey(KeyCode.LeftArrow);
+		if (goRight && !goLeft)
+		{
+			return x + speed * Time.deltaTime;
+		}
+		else if (goLeft && !goRight)
+		{
+			return x - speed * Time.deltaTime;
+		}
+		return x;
+	}
+
+}
